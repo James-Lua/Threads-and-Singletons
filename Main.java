ee7560ff54ef57112b6a12ba03b72a0ee756ff92
James <143131524+James-Lua@users.noreply.github.com>
2025-03-11 15:14:47 -0700

    Update and rename SingletonThreadTest.java to Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class SingletonThreadTest {
+public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             Thread thread = new SingletonThread();
